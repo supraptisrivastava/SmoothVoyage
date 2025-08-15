9c95d6d4a1028ea764fdaedd3314fdc192c75f32
suprapti <supsri2005@gmail.com>
2025-08-15 14:05:52 +0530

    Add start script and update server.js for Railway

--- server/server.js
+++ server/server.js
@@ -102,7 +102,11 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on port ${PORT}`);
+// });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
